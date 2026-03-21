685655e808920347a15f77d117e8942ab651548d
worker-main <mail@groupeffect.de>
2026-03-21 23:35:50 +0100

    test: geos

--- tests/settings.py
+++ tests/settings.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
-
+from sensorthings.defaults import INSTALLED_APPS as ST_INSTALLED_APPS
 
 ALLOWED_HOSTS = ["*"]
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 
-INSTALLED_APPS = ["sensorthings"]
+INSTALLED_APPS = [*ST_INSTALLED_APPS, "sensorthings"]
 
 ROOT_URLCONF = "tests.urls"
 
@@ -18,4 +18,3 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_FAKE_SECURE", "true")
 USE_TZ = True
 
 SENSORTHING_API_PREFIX = "api/sensor"
-
