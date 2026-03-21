8b4d6bd153081354083cc18c2ccc0653f23ccee8
worker-main <mail@groupeffect.de>
2026-03-21 23:14:25 +0100

    removed  imports

--- src/sensorthings/conf.py
+++ src/sensorthings/conf.py
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
-from re import Pattern
-from typing import cast
-
 from django.conf import settings
-
-from sensorthings.defaults import default_headers, default_methods
 
 
 class Settings:
@@ -17,8 +11,10 @@ class Settings:
     @property
     def SENSORTHING_API_PREFIX(self) -> str:
         return getattr(settings, "SENSORTHING_API_PREFIX", "sensorthings")
+
     @property
     def SENSORTHING_DISABLE_MODELS(self) -> bool:
         return getattr(settings, "SENSORTHING_DISABLE_MODELS", False)
+
 
 conf = Settings()
