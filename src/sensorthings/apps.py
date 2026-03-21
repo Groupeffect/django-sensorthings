6d35f6fd2e8d4b32d2289d2a6611bbc7997bc1d9
worker-main <mail@groupeffect.de>
2026-03-22 00:32:27 +0100

    test: app

--- src/sensorthings/apps.py
+++ src/sensorthings/apps.py
@@ -1,14 +1,8 @@
 from __future__ import annotations
 
 from django.apps import AppConfig
-from django.core.checks import Tags, register
-
-from sensorthings.checks import check_settings
 
 
 class SensorthingsAppConfig(AppConfig):
     name = "sensorthings"
     verbose_name = "django-sensorthings"
-
-    def ready(self) -> None:
-        register(Tags.security)(check_settings)
