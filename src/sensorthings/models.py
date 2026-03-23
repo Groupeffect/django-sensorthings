4013b902459b8666fe5332d3cec5c7fe65cde569
worker-main <mail@groupeffect.de>
2026-03-23 20:18:23 +0100

    auto update Mon Mar 23 08:18:23 PM CET 2026

--- src/sensorthings/models.py
+++ src/sensorthings/models.py
@@ -5,10 +5,10 @@ bn = {"blank": True, "null": True}
 
 
 class MetaModel(models.Model):
-    if settings.SENSORTHINGS_ENABLE_PUBLIC_PRIVATE:
+    if settings.SENSORTHINGS_ENABLE_PUBLIC_PRIVATE is not False:
         is_public = models.BooleanField(default=True)
         is_private = models.BooleanField(default=False)
-    if settings.SENSORTHINGS_ENABLE_OWNER:
+    if settings.SENSORTHINGS_ENABLE_OWNER is not False:
         has_owner = models.ForeignKey(
             settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **bn
         )
